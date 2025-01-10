740ef4dcae841ae8a2eddc5017ce59e89c32dcff
Mateusz <42359447+dotanek@users.noreply.github.com>
2025-01-10 20:50:34 +0100

    fix: general bugfixes (#18)

--- src/modules/twitch/util/message-sanitization.util.ts
+++ src/modules/twitch/util/message-sanitization.util.ts
@@ -1,5 +1,5 @@
 export class MessageSanitizationUtil {
-  private static readonly allowedCharactersRegex = new RegExp(/[^!A-Za-z0-9\s\-%]/g);
+  private static readonly allowedCharactersRegex = new RegExp(/[^!A-Za-z0-9\s\-%_]/g);
 
   static sanitize(message: string): string {
     return message.replace(this.allowedCharactersRegex, '');
--- src/modules/twitch/value-objects/twitch-context.ts
+++ src/modules/twitch/value-objects/twitch-context.ts
@@ -10,16 +10,8 @@ export class TwitchContext {
 
   static create(channel: string, userstate: ChatUserstate): TwitchContext {
     const chatRoom = new ChatRoom(channel);
-    const chatUser = new TwitchUser(
-      userstate['user-id'] || null,
-      userstate.username || 'undefined-name',
-      this._isMod(userstate),
-    );
+    const chatUser = TwitchUser.create(userstate);
 
     return new TwitchContext(chatUser, chatRoom);
-  }
-
-  private static _isMod(userstate: ChatUserstate): boolean {
-    return !!userstate.badges?.broadcaster || !!userstate.badges?.moderator;
   }
 }
